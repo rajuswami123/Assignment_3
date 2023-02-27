3d64c0842fdc6ccb11370f6e0b8b3dc982acd16e
rajuswami123 <raju.swamireddy@tringapps.com>
2023-02-27 23:27:53 +0530

    third commit

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.lang.runtime.SwitchBootstraps;
 
 class Student {
     String name;
@@ -14,22 +13,19 @@ class Student {
     }
 
     float ugpa(String ugrade) {
-        if (ugrade.equals("A+")) return 4.3F;
-        else if (ugrade.equals("A-")) return 3.7F;
-        else if (ugrade.equals("A")) return 4.0F;
-        else if (ugrade.equals("B+")) return 3.3F;
-        else if (ugrade.equals("B-")) return 2.7F;
-        else if (ugrade.equals("B")) return 3.0F;
-        else if (ugrade.equals("C+")) return 2.3F;
-        else if (ugrade.equals("C-")) return 1.7F;
-        else if (ugrade.equals("C")) return 2.0F;
-        else if (ugrade.equals("D+")) return 1.3F;
-        else if (ugrade.equals("D-")) return 0.7F;
-        else if (ugrade.equals("D")) return 1.0F;
-        else  return 0;
+            if (ugrade.equals("A+")) return 4.3F;
+            else if (ugrade.equals("A")) return 4.0F;
+            else if (ugrade.equals("B+")) return 3.3F;
 
-    }
+            else if (ugrade.equals("B")) return 3.0F;
+            else if (ugrade.equals("C+")) return 2.3F;
 
+            else if (ugrade.equals("C")) return 2.0F;
+            else if (ugrade.equals("D+")) return 1.3F;
+            else if (ugrade.equals("D")) return 1.0F;
+            else return 0;
+
+        }
     String msg(String name, float x) {
         return (name + " has a" + x + " gpa");
     }
