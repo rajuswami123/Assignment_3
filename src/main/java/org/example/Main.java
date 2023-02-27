670132d8d992f7825d39fb82e78ff6a92296b63a
rajuswami123 <raju.swamireddy@tringapps.com>
2023-02-27 23:15:29 +0530

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,34 +3,6 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-class Student{
-    String name;
-    char grade;
-    float gpa;
-    Student(String name,char grade,float gpa){
-        this.name=name;
-        this.grade=grade;
-        this.gpa=gpa;
-    }
-    float ugpa(String ugrade){
-        if(ugrade.equals("A+") ) return  4.3F;
-        if(ugrade.equals("A-") ) return 3.7F;
-        if(ugrade.equals("A")) return 4.0F;
-        if(ugrade.equals("B+")) return 3.3F;
-        if(ugrade.equals("B-") )return 2.7F;
-        if(ugrade.equals("B"))return 3.0F;
-        if(ugrade.equals("C+")) return 2.3F;
-        if(ugrade.equals("C-") )return 1.7F;
-        if(ugrade.equals("C") )return 2.0F;
-        if(ugrade.equals("D+") )return 1.3F;
-        if(ugrade.equals("D-") )return 0.7F;
-        if(ugrade.equals("D") )return 1.0F;
-        return 0;
-    }
-    String msg(String name,float x){
-        return(name+ " has a" +x+ " gpa");
-    }
-}
 public class Main {
     public static void main(String[] args) {
         Logger logger=Logger.getLogger("com.api.jar");
